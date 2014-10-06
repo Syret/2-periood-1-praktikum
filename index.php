469a8035ed5733eb86b9405dd577c6450b8f9e89
Syret <kart.syret@khk.ee>
2014-10-06 12:20:21 +0300

    Commit

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title></title>
+  <title>2. periood I praktikum</title>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="style.css" type="text/css">
 
 </head>
 <body>
   <h1>First page</h1>
+    <?php
+      $link_file = "sec_page.php";
+      $age = 32;
+    ?>
+   
+   <a href="<?php echo "{$link_file}?age={$age}"; ?>">Teisele lehele</a><br>
+    <?php 
+      print_r($_GET); echo "<br>";
+      echo $_GET["name"];
+      $jada = array("syret", "21"); echo "<br>";
+      echo $jada[1];
+    ?>
+</a>
 </body>
 </html>
--- sec_page.php
+++ sec_page.php
@@ -1,12 +1,16 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title></title>
+  <title>2. periood I praktikum</title>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="style.css" type="text/css">
 
 </head>
 <body>
 	<h1>Second page</h1>
+	<a href="index.php?name=syret&age=21">First page</a><br>
+	 <?php 
+      print_r($_GET);
+    ?>
 </body>
 </html>
